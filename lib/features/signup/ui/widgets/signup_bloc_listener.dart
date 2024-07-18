f1bdedae957761df1e5b4081edc2c8a7e7db39dc
OmarAmeer96 <omar.ameer244@gmail.com>
2024-07-18 21:21:24 +0300

    feat: Add success dialog after successful signup
    - Added a new method `showSuccessDialog` to display a dialog box with a success message after a successful signup.
    - The dialog box includes a "Continue" button that navigates the user to the login view.

--- lib/features/signup/ui/widgets/signup_bloc_listener.dart
+++ lib/features/signup/ui/widgets/signup_bloc_listener.dart
@@ -30,7 +30,8 @@ class SignupBlocListener extends StatelessWidget {
           },
           success: (signupResponse) {
             context.pop();
-            context.pushNamed(Routes.homeView);
+            // context.pushNamed(Routes.homeView);
+            showSuccessDialog(context);
           },
           error: (error) {
             context.pop();
@@ -39,6 +40,37 @@ class SignupBlocListener extends StatelessWidget {
         );
       },
       child: const SizedBox.shrink(),
+    );
+  }
+
+  void showSuccessDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Signup Successful'),
+          content: const SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Congratulations, you have signed up successfully!'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.blue,
+                disabledForegroundColor: Colors.grey.withOpacity(0.38),
+              ),
+              onPressed: () {
+                context.pushNamed(Routes.loginView);
+              },
+              child: const Text('Continue'),
+            ),
+          ],
+        );
+      },
     );
   }
 
