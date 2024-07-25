5a15c5ad7928c93be48cc9981c4e67b50e4936e6
OmarAmeer96 <omar.ameer244@gmail.com>
2024-07-25 14:56:40 +0300

    Refactor the Doctor Speciality List View.

--- lib/features/home/ui/widgets/doctor_speciality_widget.dart
+++ lib/features/home/ui/widgets/doctor_speciality_widget.dart
@@ -4,8 +4,8 @@ import 'package:appointment_app/features/home/ui/widgets/doctor_speciality_item.
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class DoctorSpecialityWidget extends StatelessWidget {
-  const DoctorSpecialityWidget({super.key});
+class DoctorSpecialityListView extends StatelessWidget {
+  const DoctorSpecialityListView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,28 +16,22 @@ class DoctorSpecialityWidget extends StatelessWidget {
           seeAllOnTap: () {},
         ),
         verticalSpace(16.h),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 10.w),
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              DoctorSpecialityItem(
-                specialityString: 'assets/images/speciality_1.png',
-                specialityText: "General",
-              ),
-              DoctorSpecialityItem(
-                specialityString: 'assets/images/speciality_2.png',
-                specialityText: "Neurologic",
-              ),
-              DoctorSpecialityItem(
-                specialityString: 'assets/images/speciality_3.png',
-                specialityText: "Pediatric",
-              ),
-              DoctorSpecialityItem(
-                specialityString: 'assets/images/speciality_4.png',
-                specialityText: "Radiology",
-              ),
-            ],
+        SizedBox(
+          height: 100.h,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 8,
+            itemBuilder: (context, index) {
+              return Container(
+                padding: EdgeInsetsDirectional.only(
+                  start: index == 0 ? 0 : 24.w,
+                ),
+                child: DoctorSpecialityItem(
+                  specialityString: 'assets/images/speciality_1.png',
+                  specialityText: "General",
+                ),
+              );
+            },
           ),
         ),
       ],
