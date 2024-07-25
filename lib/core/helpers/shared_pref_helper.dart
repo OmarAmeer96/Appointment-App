a549cfd92b72b2c344780ee1350ce0d1d66298a9
OmarAmeer96 <omar.ameer244@gmail.com>
2024-07-26 00:32:53 +0300

    refactor: Update debugPrint statements in SharedPrefHelper

--- lib/core/helpers/shared_pref_helper.dart
+++ lib/core/helpers/shared_pref_helper.dart
@@ -8,14 +8,14 @@ class SharedPrefHelper {
 
   /// Removes a value from SharedPreferences with given [key].
   static removeData(String key) async {
-    debugPrint('SharedPrefHelper : data with key : $key has been removed');
+    debugPrint('SharedPrefHelper: data with key: $key has been removed');
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.remove(key);
   }
 
   /// Removes all keys and values in the SharedPreferences
   static clearAllData() async {
-    debugPrint('SharedPrefHelper : all data has been cleared');
+    debugPrint('SharedPrefHelper: all data has been cleared');
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.clear();
   }
@@ -23,7 +23,7 @@ class SharedPrefHelper {
   /// Saves a [value] with a [key] in the SharedPreferences.
   static setData(String key, value) async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    debugPrint("SharedPrefHelper : setData with key : $key and value : $value");
+    debugPrint("SharedPrefHelper: setData with key: $key and value: $value");
     switch (value.runtimeType) {
       case String:
         await sharedPreferences.setString(key, value);
@@ -44,28 +44,28 @@ class SharedPrefHelper {
 
   /// Gets a bool value from SharedPreferences with given [key].
   static getBool(String key) async {
-    debugPrint('SharedPrefHelper : getBool with key : $key');
+    debugPrint('SharedPrefHelper: getBool with key: $key');
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences.getBool(key) ?? false;
   }
 
   /// Gets a double value from SharedPreferences with given [key].
   static getDouble(String key) async {
-    debugPrint('SharedPrefHelper : getDouble with key : $key');
+    debugPrint('SharedPrefHelper: getDouble with key: $key');
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences.getDouble(key) ?? 0.0;
   }
 
   /// Gets an int value from SharedPreferences with given [key].
   static getInt(String key) async {
-    debugPrint('SharedPrefHelper : getInt with key : $key');
+    debugPrint('SharedPrefHelper: getInt with key: $key');
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences.getInt(key) ?? 0;
   }
 
   /// Gets an String value from SharedPreferences with given [key].
   static getString(String key) async {
-    debugPrint('SharedPrefHelper : getString with key : $key');
+    debugPrint('SharedPrefHelper: getString with key: $key');
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences.getString(key) ?? '';
   }
@@ -74,20 +74,20 @@ class SharedPrefHelper {
   static setSecuredString(String key, String value) async {
     const flutterSecureStorage = FlutterSecureStorage();
     debugPrint(
-        "FlutterSecureStorage : setSecuredString with key : $key and value : $value");
+        "FlutterSecureStorage: setSecuredString with key: $key and value: $value");
     await flutterSecureStorage.write(key: key, value: value);
   }
 
   /// Gets an String value from FlutterSecureStorage with given [key].
   static getSecuredString(String key) async {
     const flutterSecureStorage = FlutterSecureStorage();
-    debugPrint('FlutterSecureStorage : getSecuredString with key :');
+    debugPrint('FlutterSecureStorage: getSecuredString with key :');
     return await flutterSecureStorage.read(key: key) ?? '';
   }
 
   /// Removes all keys and values in the FlutterSecureStorage
   static clearAllSecuredData() async {
-    debugPrint('FlutterSecureStorage : all data has been cleared');
+    debugPrint('FlutterSecureStorage: all data has been cleared');
     const flutterSecureStorage = FlutterSecureStorage();
     await flutterSecureStorage.deleteAll();
   }
