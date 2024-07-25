31f00627e1c1189547f1598742d407445de0f560
OmarAmeer96 <omar.ameer244@gmail.com>
2024-07-25 14:04:42 +0300

    Refactored the CustomSectionHeader.

--- lib/features/home/ui/widgets/doctor_speciality_widget.dart
+++ lib/features/home/ui/widgets/doctor_speciality_widget.dart
@@ -1,7 +1,5 @@
 import 'package:appointment_app/core/helpers/spacing.dart';
-import 'package:appointment_app/core/theming/colors_manager.dart';
-import 'package:appointment_app/core/theming/font_family_helper.dart';
-import 'package:appointment_app/core/theming/styles.dart';
+import 'package:appointment_app/core/widgets/custom_section_header.dart';
 import 'package:appointment_app/features/home/ui/widgets/doctor_speciality_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -13,27 +11,9 @@ class DoctorSpecialityWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          children: [
-            Text(
-              'Doctor Speciality',
-              style: Styles.font18BlackBold.copyWith(
-                fontSize: 18.sp,
-              ),
-            ),
-            const Spacer(),
-            GestureDetector(
-              onTap: () {},
-              child: Text(
-                'See All',
-                style: Styles.font18BlackBold.copyWith(
-                  fontSize: 13.sp,
-                  color: ColorsManager.mainBlue,
-                  fontFamily: FontFamilyHelper.regular,
-                ),
-              ),
-            ),
-          ],
+        CustomSectionHeader(
+          title: 'Doctor Speciality',
+          seeAllOnTap: () {},
         ),
         verticalSpace(16.h),
         Padding(
